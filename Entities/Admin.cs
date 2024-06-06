2839b5716b41805b27e76b4e10d6feb6d9a0c1e8
Guilherme Simon <guilhermefilheiro@gmail.com>
2024-06-06 14:21:58 -0300

    Criado a Classe User e Admin

--- Entities/Admin.cs
+++ Entities/Admin.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Biblioteca.Entities
 {
-    internal class Admin
+    public enum SystemRoles
     {
+        viewer,
+        editor,
+        owner
+    }
+    public class Admin : User
+    {
+        public SystemRoles AcessLevel { get; set; }
+
+        public Admin(string name, string email, int id, SystemRoles accessLevel) : base(name, email, id)
+        {
+            AcessLevel = accessLevel;
+        }
+
+        public override void DisplayDetails()
+        {
+            Console.WriteLine($"Id: {Id}\nNome: {Name}\nEmail: {Email}\nNivel de Acesso: {AcessLevel}");
+        }
     }
 }
