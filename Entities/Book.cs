e64b985d4c07ce14a3ef226069998b2e563f7a69
gian.braga <gian.braga@vortigo.com.br>
2024-06-06 13:43:32 -0300

    completando classe book;ebook

--- Entities/Book.cs
+++ Entities/Book.cs
@@ -4,9 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Biblioteca
+namespace Biblioteca.Entities
 {
     internal class Book
     {
+        protected string Title { get;  private set; }
+        protected string Author { get; private set; }
+        protected int ISBN { get; private set; }
+
+        public Book(string title, string author, int isbm)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.ISBN = isbm;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("-----DETALHES DO LIVRO-----");
+            Console.WriteLine($"TITULO: {this.Title}");
+            Console.WriteLine($"AUTOR: {this.Author}");
+            Console.WriteLine($"ISBN: {this.ISBN}");
+        }
+
+
+
     }
 }
--- Entities/Ebook.cs
+++ Entities/Ebook.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Biblioteca
+namespace Biblioteca.Entities
 {
     internal class Ebook : Book
     {
+        protected double FileSize { get; private set; }
+        protected string FileFormat { get; private set; }
+        public Ebook(double filesize, string fileformat, string title, string author, int isbm) : base(title, author, isbm)
+        {
+            this.FileSize = filesize;
+            this.FileFormat = fileformat;
+
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("-----DETALHES DO EBOOK-----");
+            Console.WriteLine($"TITULO: {this.Title}");
+            Console.WriteLine($"AUTOR: {this.Author}");
+            Console.WriteLine($"TAMANHO DO ARQUIVO: {this.FileSize}");
+            Console.WriteLine($"FORMATO DO ARQUIVO: {this.FileFormat}");
+            Console.WriteLine($"ISBN: {this.ISBN}");
+        }
     }
 }
